5ba9f438f3a921f35944337c7d734fc4f8b2ac7b
Alfianwijayanto24 <alvianwijayanto74@gmail.com>
2026-05-12 11:13:33 +0700

    keluar dan enter your choice

--- stakusingenklist.cpp
+++ stakusingenklist.cpp
@@ -88,4 +88,8 @@ while (choice != 5)
         cout << "1. Push" << endl;
         cout << "2. Pop" << endl;
         cout << "3. Peek" << endl;
-}
+        cout << "4. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+}};
